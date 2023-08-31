e1fa27a4ae51c354c75110fc3b539d6b5736bac5
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-31 14:35:55 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Greeting from './components/greeting';
 
-const App = () => <Greeting />;
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Greeting />} />
+  </Routes>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
@@ -7,7 +8,9 @@ import store from './redux/store';
 function Index() {
   return (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 }
